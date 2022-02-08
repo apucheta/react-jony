a8b9adad2dd672fb618822e0061cbb51bc851c60
apucheta <agustin.s.pucheta@gmail.com>
2022-02-08 11:46:47 -0300

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+// importaciones
+import React from 'react';
+
+// El componente en si
+const Header = () => {
+  //codigo js
+  //vista
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  );
+};
+
+// Exportacion
+export default Header;
