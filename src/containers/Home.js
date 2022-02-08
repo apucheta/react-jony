55725505691b6c2c8f7b163b08d3a2cd464e257e
apucheta <agustin.s.pucheta@gmail.com>
2022-02-08 20:48:35 -0300

    routing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/css/Home.css';
 
 export default function Home() {
   return (
     <div>
+      <header>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/somos'>Somos</Link>
+          </li>
+          <li>
+            <Link to='/contacto'>Contacto</Link>
+          </li>
+        </ul>
+      </header>
       <h1>Hola, soy el home</h1>
     </div>
   );
